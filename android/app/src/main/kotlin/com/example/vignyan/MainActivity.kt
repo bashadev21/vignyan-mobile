e75b970ee7d7550b64db232d856c90252b338d13
bashadev21 <basha28062@gmail.com>
2022-09-03 20:55:05 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/vignyan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vignyan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/readme_before_analyze.dart
@@ -0,0 +1,9 @@
+//This is school management-mobile applications containes student and teacher//
+//Developed By - Teckzy Team
+//This project created flutter version is --> 3.0.5
+//STATE-MANAGEMET ==>Getx
+//Local storage==> Get storage
+//FOLDER==>MVC Pattern
+//If file name starts with s_ its student related file ,then starts with _t its teacher related file
+//Example:s_bottom_bar.dart , t_bootom_bar.dart
+//
